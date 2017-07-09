8c1bfea981baae6208e6bca9e2b76014abbdf22f
Godley <charlotte.godley@hotmail.co.uk>
2017-07-09 23:33:37 +0100

    tests: add namespace name to list_namespaced_pod since I missed that

--- pytest/test_deployment.py
+++ pytest/test_deployment.py
@@ -9,20 +9,22 @@ def test_create(deployment):
     pass
 
 @pytest.second
-def test_scheduled(deployment):
+def test_scheduled(deployment, namespace):
     '''should check created deployment has scheduled pods'''
     client = kubernetes.client.CoreV1Api()
     w = kubernetes.watch.Watch()
-    for event in w.stream(client.list_namespaced_pod, labels=deployment.spec.template.spec.metadata.labels,
+    for event in w.stream(client.list_namespaced_pod, namespace.metadata.name,
+                          labels=deployment.spec.template.spec.metadata.labels,
                           _request_timeout=60):
         pass
 
 @pytest.third
-def test_running(deployment):
+def test_running(deployment, namespace):
     '''should check scheduled pods started and are running'''
     client = kubernetes.client.CoreV1Api()
     w = kubernetes.watch.Watch()
-    for event in w.stream(client.list_namespaced_pod, labels=deployment.spec.template.spec.metadata.labels,
+    for event in w.stream(client.list_namespaced_pod, namespace.metadata.name,
+                          labels=deployment.spec.template.spec.metadata.labels,
                           _request_timeout=60):
         pass
 
