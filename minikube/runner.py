3d650d1b3016121a5e0d90f13a24761a06262da0
Godley <charlotte.godley@hotmail.co.uk>
2017-07-11 20:25:50 +0100

    feat: add code for runner + kubeconfig for pytest

--- minikube/runner.py
+++ minikube/runner.py
@@ -1,1 +1,16 @@
-__author__ = 'charlottegodley'
+import kubernetes
+
+def main():
+    namespace = kubernetes.client.V1Namespace(
+        metadata=kubernetes.client.V1ObjectMeta(
+            name="mynamespace"
+        )
+    )
+    kubernetes.config.load_incluster_config()
+    v1 = kubernetes.client.CoreV1Api()
+    v1.create_namespace(namespace)
+    v1.read_namespace(namespace.metadata.name)
+    v1.delete_namespace(namespace.metadata.name, kubernetes.client.V1DeleteOptions())
+
+if __name__ == "__main__":
+    main()
--- pytest/conftest.py
+++ pytest/conftest.py
@@ -1,9 +1,13 @@
-from kubernetes import client
+from kubernetes import client, config
 import pytest
 
 
+def configure_kube():
+    config.load_kube_config()
+
 @pytest.fixture()
 def namespace():
+    configure_kube()
     return client.V1Namespace(
         metadata=client.V1ObjectMeta(name="helloworld")
     )
@@ -11,6 +15,7 @@ def namespace():
 
 @pytest.fixture()
 def deployment():
+    configure_kube()
     return client.AppsV1beta1Deployment(
         metadata=client.V1ObjectMeta(name="helloworld",
                                      namespace="helloworld",
@@ -21,6 +26,7 @@ def deployment():
 
 @pytest.fixture()
 def service():
+    configure_kube()
     return client.V1Service(
         metadata=client.V1ObjectMeta(name="app",
                                      namespace="helloworld"),
